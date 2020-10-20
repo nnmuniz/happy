f6178a944709d5689bd82c9278237efb3ec5171a
nnmuniz <pcnathalya@hotmail.com>
2020-10-20 12:59:46 -0300

    Latitude e longitude de forma dinamica

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -6,8 +6,13 @@ const options = {
     zoonControl: false
 }
 
+// get values from html 
+const lat = document.querySelector('span[data-lat]').dataset.lat;
+const lng = document.querySelector('span[data-lng]').dataset.lng;
+
+
 //create map
-const map = L.map('mapid', options).setView([-23.644602,-46.5619968], 13);
+const map = L.map('mapid', options).setView([lat,lng], 13);
 
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
@@ -22,8 +27,9 @@ const icon = L.icon({
 })
 
 //create and add marker
+
 L
-.marker([-23.644602,-46.5619968], {icon})
+.marker([lat, lng], {icon})
 .addTo(map)
 
 function selectImage(event) {
