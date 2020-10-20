07f37202372d5cf86f502a4f26cf633cd6493971
nnmuniz <pcnathalya@hotmail.com>
2020-10-19 22:23:11 -0300

    Utilização de Nodemon e Handlebar, Start Server

---
+++ src/database/mock-data.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-23.644602",
+        lng: "-46.5619968",
+        name: "Núcleo Convivência Meninas", 
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.", 
+        images: [
+            "https://images.unsplash.com/photo-1595295413110-3304e36b564f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1585338927000-1c787b17eb5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ], 
+
+        instrucions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-23.644602",
+        lng: "-46.5719968",
+        name: "Núcleo Convivência Meninos", 
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.", 
+        images: [
+            "https://images.unsplash.com/photo-1595295413110-3304e36b564f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1585338927000-1c787b17eb5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ], 
+
+        instrucions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+    }
+]
---
+++ src/server.js
@@ -0,0 +1,24 @@
+//importar dependencia
+const express = require("express");
+const path = require("path");
+const pages = require("./pages.js");
+
+//iniciar express
+const server = express();
+server
+
+  //utilizando os arquivos estaticos
+  .use(express.static("public"))
+
+  //configurar template engine
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "hbs")
+
+  // criar uma rota
+  .get("/", pages.index)
+  .get("/orphanage", pages.orphanage)
+  .get("/orphanages", pages.orphanages)
+  .get("/create-orphanage", pages.createOphanage);
+
+//ligar o servidor
+server.listen(5500);
